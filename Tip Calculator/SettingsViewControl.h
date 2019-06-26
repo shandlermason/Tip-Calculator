dc081934a352561c23f2ad3fcf59c25e21be4057
Shandler Mason <samason1@fb.com>
2019-06-25 18:08:17 -0700

    Changes- Added settings button and push navigation

---
+++ Tip Calculator/SettingsViewControl.h
@@ -0,0 +1,17 @@
+//
+//  SettingsViewControl.h
+//  Tip Calculator
+//
+//  Created by samason1 on 6/25/19.
+//  Copyright © 2019 samason1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SettingsViewControl : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tip Calculator/SettingsViewControl.m
@@ -0,0 +1,13 @@
+//
+//  SettingsViewControl.m
+//  Tip Calculator
+//
+//  Created by samason1 on 6/25/19.
+//  Copyright © 2019 samason1. All rights reserved.
+//
+
+#import "SettingsViewControl.h"
+
+@implementation SettingsViewControl
+
+@end
